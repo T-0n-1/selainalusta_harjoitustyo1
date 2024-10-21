ff979a830ff16c5e2e20ad9055ae750afa30c8f7
Toni Mertanen <t.mertanen@hotmail.com>
2024-10-21 22:10:58 +0300

    Add missing two files into prettier igore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,31 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  			{ ignores: ["venv/", ".idea/", ".git/", "node_modules/", ".vscode", ".DS_Store", "config.codekit3", ".gitignore", "dist/", "Thumbs.db", ".eslintcache", ".env", "test/", "tests/", "tsconfig.json", "*.mjs", "*.min.js"] },  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: [
+      "venv/",
+      ".idea/",
+      ".git/",
+      "node_modules/",
+      ".vscode",
+      ".DS_Store",
+      "config.codekit3",
+      ".gitignore",
+      "dist/",
+      "Thumbs.db",
+      ".eslintcache",
+      ".env",
+      "test/",
+      "tests/",
+      "tsconfig.json",
+      "*.mjs",
+      "*.min.js",
+    ],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
