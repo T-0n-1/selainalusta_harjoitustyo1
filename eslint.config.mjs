7815e37e1fd10e16757ecb007c58f27dc29e54ff
Toni Mertanen <t.mertanen@hotmail.com>
2024-10-21 22:05:20 +0300

    Bugfix in prettier ignore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,8 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  			{ ignores: ["venv/", ".idea/", ".git/", "node_modules/", ".vscode", ".DS_Store", "config.codekit3", ".gitignore", "dist/", "Thumbs.db", ".eslintcache", ".env", "tsconfig.json", "test/", "*.mjs", "*.min.js"] },  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  			{ ignores: ["venv/", ".idea/", ".git/", "node_modules/", ".vscode", ".DS_Store", "config.codekit3", ".gitignore", "dist/", "Thumbs.db", ".eslintcache", ".env", "test/", "tests/", "tsconfig.json", "*.mjs", "*.min.js"] },  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
